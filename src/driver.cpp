24faedd742ee4b6479ef967e6792b6e228b7c5ac
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-03-28 10:21:46 +0200

    Use the new lib

--- src/driver.cpp
+++ src/driver.cpp
@@ -5,37 +5,18 @@
 //  http://opensource.org/licenses/MIT)
 //=======================================================================
 
-#include <iostream>
-#include <cstdlib>
-#include <cstdio>
-#include <cstring>
+#include "asgard/driver.hpp"
+
 #include <random>
 
-#include <sys/socket.h>
-#include <sys/un.h>
-#include <unistd.h>
-#include <signal.h>
-
 namespace {
-
-const std::size_t UNIX_PATH_MAX = 108;
-const std::size_t buffer_size = 4096;
 
 // Configuration (this should be in a configuration file)
 const char* server_socket_path = "/tmp/asgard_socket";
 const char* client_socket_path = "/tmp/asgard_random_socket";
 const std::size_t delay_ms = 5000;
 
-//Buffer
-char write_buffer[buffer_size];
-char receive_buffer[buffer_size];
-
-// The socket file descriptor
-int socket_fd;
-
-// The socket addresses
-struct sockaddr_un client_address;
-struct sockaddr_un server_address;
+asgard::driver_connector driver;
 
 // The remote IDs
 int source_id = -1;
@@ -44,23 +25,14 @@ int sensor_id = -1;
 void stop(){
     std::cout << "asgard:random: stop the driver" << std::endl;
 
-    // Unregister the sensor, if necessary
-    if(sensor_id >= 0){
-        auto nbytes = snprintf(write_buffer, buffer_size, "UNREG_SENSOR %d %d", source_id, sensor_id);
-        sendto(socket_fd, write_buffer, nbytes, 0, (struct sockaddr *) &server_address, sizeof(struct sockaddr_un));
-    }
-
-    // Unregister the source, if necessary
-    if(source_id >= 0){
-        auto nbytes = snprintf(write_buffer, buffer_size, "UNREG_SOURCE %d", source_id);
-        sendto(socket_fd, write_buffer, nbytes, 0, (struct sockaddr *) &server_address, sizeof(struct sockaddr_un));
-    }
+    asgard::unregister_sensor(driver, source_id, sensor_id);
+    asgard::unregister_source(driver, source_id);
 
     // Unlink the client socket
     unlink(client_socket_path);
 
     // Close the socket
-    close(socket_fd);
+    close(driver.socket_fd);
 }
 
 void terminate(int){
@@ -72,24 +44,8 @@ void terminate(int){
 } //End of anonymous namespace
 
 int main(){
-    // Open the socket
-    socket_fd = socket(AF_UNIX, SOCK_DGRAM, 0);
-    if(socket_fd < 0){
-        std::cerr << "asgard:random: socket() failed" << std::endl;
-        return 1;
-    }
-
-    // Init the client address
-    memset(&client_address, 0, sizeof(struct sockaddr_un));
-    client_address.sun_family = AF_UNIX;
-    snprintf(client_address.sun_path, UNIX_PATH_MAX, client_socket_path);
-
-    // Unlink the client socket
-    unlink(client_socket_path);
-
-    // Bind to client socket
-    if(bind(socket_fd, (const struct sockaddr *) &client_address, sizeof(struct sockaddr_un)) < 0){
-        std::cerr << "asgard:random: bind() failed" << std::endl;
+    // Open the connection
+    if(!asgard::open_driver_connection(driver, client_socket_path, server_socket_path)){
         return 1;
     }
 
@@ -97,34 +53,9 @@ int main(){
     signal(SIGTERM, terminate);
     signal(SIGINT, terminate);
 
-    // Init the server address
-    memset(&server_address, 0, sizeof(struct sockaddr_un));
-    server_address.sun_family = AF_UNIX;
-    snprintf(server_address.sun_path, UNIX_PATH_MAX, server_socket_path);
-
-    socklen_t address_length = sizeof(struct sockaddr_un);
-
-    // Register the source
-    auto nbytes = snprintf(write_buffer, buffer_size, "REG_SOURCE random");
-    sendto(socket_fd, write_buffer, nbytes, 0, (struct sockaddr *) &server_address, sizeof(struct sockaddr_un));
-
-    auto bytes_received = recvfrom(socket_fd, receive_buffer, buffer_size, 0, (struct sockaddr *) &(server_address), &address_length);
-    receive_buffer[bytes_received] = '\0';
-
-    source_id = atoi(receive_buffer);
-
-    std::cout << "asgard:random: remote source: " << source_id << std::endl;
-
-    // Register the sensor
-    nbytes = snprintf(write_buffer, buffer_size, "REG_SENSOR %d %s %s", source_id, "RANDOM", "rand_100");
-    sendto(socket_fd, write_buffer, nbytes, 0, (struct sockaddr *) &server_address, sizeof(struct sockaddr_un));
-
-    bytes_received = recvfrom(socket_fd, receive_buffer, buffer_size, 0, (struct sockaddr *) &(server_address), &address_length);
-    receive_buffer[bytes_received] = '\0';
-
-    sensor_id = atoi(receive_buffer);
-
-    std::cout << "asgard:random: remote sensor: " << sensor_id << std::endl;
+    // Register the source and sensors
+    source_id = asgard::register_source(driver, "random");
+    sensor_id = asgard::register_sensor(driver, source_id, "RANDOM", "rand_100");
 
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -133,9 +64,7 @@ int main(){
     while(true){
         int value = distribution(gen);
 
-        // Send the data
-        nbytes = snprintf(write_buffer, buffer_size, "DATA %d %d %d", source_id, sensor_id, value);
-        sendto(socket_fd, write_buffer, nbytes, 0, (struct sockaddr *) &server_address, sizeof(struct sockaddr_un));
+        asgard::send_data(driver, source_id, sensor_id, value);
 
         // Wait some time before messages
         usleep(delay_ms * 1000);
